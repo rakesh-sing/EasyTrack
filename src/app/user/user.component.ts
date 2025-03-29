195a745b986314cb4dd58d381f12b805fb1b7e48
Rakesh Sing <rakesh-sing@outlook.com>
2025-03-30 01:19:52 +0530

    signal implementation

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
@@ -9,14 +9,28 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  //start ofZone.js Change detection stratigy
+  // selectedUser = DUMMY_USERS[randomIndex];
 
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
+
+  // selecteUser() {
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   this.selectedUser = DUMMY_USERS[randomIndex];
+  // }
+
+  //End of Zone.js Change detection stratigy
+
+  //Use of Signal
+
+  selectedUser = signal(DUMMY_USERS[randomIndex]); //Signal initial value passing or Signal initialization using signal()
+
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar); // Signal value reaading here using computed()
 
   selecteUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]); //Signal value changing here or Signal Value updating using set()
   }
 }
