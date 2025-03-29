df683c986e8cda906fb5dc8cd26910b795623a91
Rakesh Sing <rakesh-sing@outlook.com>
2025-03-29 17:58:32 +0530

    use of zone.js for state and change detection

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -14,4 +14,9 @@ export class UserComponent {
   get imagePath() {
     return 'assets/users/' + this.selectedUser.avatar;
   }
+
+  selecteUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
 }
